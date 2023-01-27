14abd8ed51f8cbce4d47b0c134a4f81d7efe2ec4
Saha Kuljit Shantanu <81855404+Saha-Kuljit-Shantanu@users.noreply.github.com>
2023-01-27 10:10:01 +0600

    Add files via upload

---
+++ OFFLINE_1_STRUCTURAL_DESIGN_PATTERNS/PROBLEM_2/INTERFACES/Engine.java
@@ -0,0 +1,7 @@
+public interface Engine {
+
+    void setEngine(String engine);
+    String getEngine();
+
+
+}
---
+++ OFFLINE_1_STRUCTURAL_DESIGN_PATTERNS/PROBLEM_2/INTERFACES/Model.java
@@ -0,0 +1,6 @@
+public interface Model {
+
+    void setName(String name);
+    String getName();
+
+}
