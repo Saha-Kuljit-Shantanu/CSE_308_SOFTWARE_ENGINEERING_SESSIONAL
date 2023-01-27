ec762cebfddfdf72947e01c208d1478e7aaec1fb
Saha Kuljit Shantanu <81855404+Saha-Kuljit-Shantanu@users.noreply.github.com>
2023-01-27 10:13:21 +0600

    Add files via upload

---
+++ OFFLINE_1_STRUCTURAL_DESIGN_PATTERNS/PROBLEM_2/CONCRETE_CAR_CLASSES/Tesla.java
@@ -0,0 +1,11 @@
+public class Tesla extends Car{
+
+    public Tesla(){
+
+        setName("Tesla");
+        setColor("White");
+        setCountry("US");
+        setEngine("Electric engine");
+        setDriveTrain("All wheels");
+    }
+}
